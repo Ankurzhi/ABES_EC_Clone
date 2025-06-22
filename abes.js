7a667e6841204027ce00f45a3d5f569d84e9bf20
Ankurzhi <ankubabu1972@gmail.com>
2025-06-22 18:24:28 +0530

    Initial commit

---
+++ abes.js
@@ -0,0 +1,23 @@
+
+function ABES() {
+
+        document.getElementById("btn").addEventListener("click", function () {
+
+                window.alert('Welcome to home page');
+                window.location.href = "ABES.html";
+        });
+
+}
+
+function closePopup() {
+        document.getElementById("popupOverlay").style.display = "none";
+}
+function popup() {
+
+        document.getElementById("popimg").addEventListener("click", function () {
+
+
+                window.location.href = "Contact_us.html";
+        });
+
+}
